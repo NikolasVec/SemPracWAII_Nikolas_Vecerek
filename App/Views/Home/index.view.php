9a0c2e237529e5bbf87aad92e49044383d30b481
NikolasVec <nikove17@gmail.com>
2026-01-17 14:38:15 +0100

    Uprava dizajnu buniek

--- App/Views/Home/index.view.php
+++ App/Views/Home/index.view.php
@@ -5,7 +5,7 @@
 <div class="container-fluid p-0 m-0">
     <div class="row">
             <div class="text-center p-0 m-0 position-relative">
-                <img src="<?= $link->asset('images/BEHPOPIVO_OBR_BEZTEXTU.png') ?>" class="w-100"
+                <img src="<?= $link->asset('images/BEHPOPIVO_OBR_BEZTEXTU.png') ?>" alt="Banner" class="w-100"
                      style="display:block;margin:0;padding:0;border:0;">
             <div class="text-center p-0 m-0 home-image-container">
                 <div class="behpopivo-nadpis">BEH PO PIVO</div>
@@ -29,44 +29,33 @@
         <div class="col-12">
             <div class="row">
                 <div class="col-md-4 p-2">
-                    <a href="<?= $link->url('home.informacie') ?>" class="text-decoration-none">
-                        <div class="p-4 border h-100 d-flex flex-column justify-content-center align-items-start bg-light">
+                    <a href="<?= $link->url('home.informacie') ?>" class="text-decoration-none text-dark">
+                        <div class="p-4 border h-100 d-flex flex-column justify-content-center align-items-center text-center text-dark" style="background-color:#ffce0a;">
+                            <img src="<?= $link->asset('images/Bezec.jpg') ?>" alt="Bežec" class="img-fluid mb-2" style="max-height:160px;object-fit:contain;">
                             <h5>Informácie a pravidlá</h5>
                             <p class="mb-0">Všetky informácie a pravidlá behu nájdete tu.</p>
                         </div>
                     </a>
                 </div>
                 <div class="col-md-4 p-2">
-                    <a href="https://pivovarmartins.sk/charakternost-nasich-piv/" target="_blank" rel="noopener" class="text-decoration-none">
-                        <div class="p-4 border h-100 d-flex flex-column justify-content-center align-items-start bg-light">
-                            <img src="<?= $link->asset('images/Pivovar-Martins.jpg') ?>" alt="Pivovar Martins" class="img-fluid mb-2" style="max-height:120px;object-fit:cover;">
-                            <h5>Pivo počas behu</h5>
-                            <p class="mb-0">Po behu budeme podávať pivo z Pivovaru Martins. Viac o sortimente nájdete na stránke pivovaru.</p>
+                    <a href="https://pivovarmartins.sk/charakternost-nasich-piv/" target="_blank" rel="noopener" class="text-decoration-none text-dark">
+                        <div class="p-4 border h-100 d-flex flex-column justify-content-center align-items-center text-center text-dark" style="background-color:#ffce0a;">
+                            <img src="<?= $link->asset('images/Pivovar-Martins.jpg') ?>" alt="Pivovar Martins" class="img-fluid mb-2" style="max-height:160px;object-fit:contain;">
+                            <h5>Čo sa bude piť</h5>
+                            <p class="mb-0">Počas behu budeme podávať pivo z Pivovaru Martins.</p>
                         </div>
                     </a>
                 </div>
                 <div class="col-md-4 p-2">
-                    <div class="p-4 border h-100 d-flex flex-column justify-content-center align-items-center bg-light text-center">
+                    <div class="p-4 border h-100 d-flex flex-column justify-content-center align-items-center text-center text-dark" style="background-color:#ffce0a;">
                         <img src="<?= $link->asset('images/Obrazok_tricko.png') ?>" alt="Tričko podujatia" class="img-fluid mb-2" style="max-height:160px;object-fit:contain;">
                         <h5>Oficiálne tričko</h5>
-                        <p class="mb-0">Pohodlné tričko s motívom podujatia — suvenír pre účastníkov.</p>
+                        <p class="mb-0">Pohodlné tričko s motívom podujatia sa da zakúpiť na štarte a na cieli behu.</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="row mt-3">
-        <div class="col text-center">
-            <h4>Authors</h4>
-            <div>
-                <a href="mailto:Patrik.Hrkut@fri.uniza.sk">doc. Ing. Patrik Hrkút, PhD.</a><br>
-                <a href="mailto:Michal.Duracik@fri.uniza.sk">Ing. Michal Ďuračík, PhD.</a><br>
-                <a href="mailto:Matej.Mesko@fri.uniza.sk">Ing. Matej Meško, PhD.</a><br><br>
-                &copy; 2020-<?= date('Y') ?> University of Žilina, Faculty of Management Science and Informatics,
-                Department of Software Technologies
-            </div>
-        </div>
-    </div>
 
 </div>
